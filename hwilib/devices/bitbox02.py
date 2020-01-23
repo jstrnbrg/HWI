846df21a716d649e96542eefa3ce488120f86395
TheCharlatan <seb.kung@gmail.com>
2020-01-23 23:45:18 +0100

    BitBox02 support signing (broken)

--- hwilib/devices/bitbox02.py
+++ hwilib/devices/bitbox02.py
@@ -4,11 +4,12 @@ from ..hwwclient import HardwareWalletClient
 from ..errors import ActionCanceledError, BadArgumentError, DeviceConnectionError, DeviceFailureError, UnavailableActionError, common_err_msgs, handle_errors
 
 import base64
+from binascii import hexlify, unhexlify
 import hid
 import struct
 from .. import base58
 from ..base58 import get_xpub_fingerprint_hex
-from ..serializations import hash256, hash160, CTransaction
+from ..serializations import hash256, hash160, CTransaction, CTxOut, ser_uint256
 import logging
 import re
 
@@ -18,6 +19,7 @@ BITBOX02_DEVICE_ID = 0x2403
 BIP32_PRIME = 0x80000000
 UINT32_MAX = (1 << 32) - 1
 
+py_enumerate = enumerate # Need to use the enumerate built-in but there's another function already named that
 
 def check_keypath(key_path):
     if key_path == "m/44'/0'/0'":
@@ -124,17 +126,25 @@ class Bitbox02Client(HardwareWalletClient):
         keypath = convert_bip32_path_to_list_of_uint32(path)
         coin_network = coin_network_from_bip32_list(keypath)
 
+        if keypath[0] == 84 + HARDENED or keypath[0] == 49 + HARDENED:
+            if self.is_testnet:
+                output_type = bitbox02.btc.BTCPubRequest.TPUB
+            else:
+                output_type = bitbox02.btc.BTCPubRequest.XPUB
+        else:
+            raise Exception("invalid keypath or address_type, only supports BIP 84 p2wpkh and BIP 49 p2wpkh_p2sh")
+
         print(path)
         xpub = self.app.btc_pub(
             keypath=keypath,
-            output_type=bitbox02.btc.BTCPubRequest.XPUB,
+            output_type=output_type,
             coin=coin_network,
             display=False,
         )
         print(xpub)
         xpub = self.app.btc_pub(
             keypath=keypath,
-            output_type=bitbox02.btc.BTCPubRequest.XPUB,
+            output_type=output_type,
             coin=coin_network,
         )
         return {'xpub': xpub}
@@ -143,6 +153,113 @@ class Bitbox02Client(HardwareWalletClient):
     # The tx must be in the combined unsigned transaction format
     # Current only supports segwit signing
     def sign_tx(self, tx):
+        coin = bitbox02.btc.BTC
+        if self.is_testnet:
+            coin = bitbox02.btc.TBTC
+
+        tx_script_type = None
+        master_fp = self.get_master_fingerprint()
+        master_fp = struct.unpack("<I", unhexlify(master_fp.encode()))[0]
+                # Build BTCInputType list
+        inputs = []
+        for input_num, (psbt_in, txin) in py_enumerate(list(zip(tx.inputs, tx.tx.vin))):
+
+            #_, full_path = keystore.find_my_pubkey_in_txinout(txin)
+            for key in psbt_in.hd_keypaths.keys():
+                full_path = list(psbt_in.hd_keypaths[key])
+                if full_path[0] == master_fp:
+                    key_path = full_path[1:]
+                    break
+
+            #if not check_keypath(full_path):
+            #    raise Exception("Invalid keypath")
+            #keypath = convert_bip32_path_to_list_of_uint32(full_path)
+            prevout_hash = ser_uint256(txin.prevout.hash)[::-1]
+            print("prevout hash", prevout_hash)
+            print("prevout index", txin.prevout.n)
+            print("value", psbt_in.witness_utxo.nValue)
+            print("sequence", txin.nSequence)
+            print("key path", key_path)
+            inputs.append(
+                {
+                    "prev_out_hash": prevout_hash,
+                    "prev_out_index": txin.prevout.n,
+                    "prev_out_value": psbt_in.witness_utxo.nValue,
+                    "sequence": txin.nSequence,
+                    "keypath": key_path,
+                }
+            )
+
+        # Build BTCOutputType list
+        outputs = []
+        for i, txout in py_enumerate(tx.tx.vout):
+            change_path = []
+            is_change = False
+
+            # check for change
+            for pubkey, path in tx.outputs[i].hd_keypaths.items():
+                if path[0] == master_fp and len(path) > 2 and path[-2] == 1:
+                    # For possible matches, check if pubkey matches possible template
+                    if hash160(pubkey) in txout.scriptPubKey or hash160(bytearray.fromhex("0014") + hash160(pubkey)) in txout.scriptPubKey:
+                        change_path = path[:-1]
+                        is_change = True
+                        break
+
+
+            if is_change:
+                print("This is the change keypath:", change_path)
+                print("This is the change value:", txout.nValue)
+                outputs.append(
+                    bitbox02.BTCOutputInternal(
+                        keypath=change_path,
+                        value=txout.nValue,
+                        )
+                )
+            else:
+                #addrtype, pubkey_hash = bitcoin.address_to_hash(txout.address)
+                #if addrtype == bitcoin.WIF_SCRIPT_TYPES["p2pkh"]:
+                #    output_type = bitbox02.btc.P2PKH
+                #elif addrtype == bitcoin.WIF_SCRIPT_TYPES["p2sh"]:
+                #    output_type = bitbox02.btc.P2SH
+                #elif addrtype == bitcoin.WIF_SCRIPT_TYPES["p2wpkh"]:
+                #    output_type = bitbox02.btc.P2WPKH
+                #elif addrtype == bitcoin.WIF_SCRIPT_TYPES["p2wsh"]:
+                #    output_type = bitbox02.btc.P2WSH
+                #else:
+                #    raise Exception(
+                #        "Received unsupported output type during transaction signing"
+                #    )
+                print("This is the txout scriptPubkey:", txout.scriptPubKey)
+                print("This is the txout value:", txout.nValue)
+                outputs.append(
+                    bitbox02.BTCOutputExternal(
+                        output_type=bitbox02.btc.P2WPKH,
+                        output_hash=txout.scriptPubKey,
+                        value=txout.nValue,
+                    )
+                )
+
+        print("This is the locktime:", tx.tx.nLockTime)
+        print("This is the version:", tx.tx.nVersion)
+        sigs = self.app.btc_sign(
+            coin,
+            bitbox02.btc.SCRIPT_P2WPKH,
+            bip44_account=full_path[2],
+            inputs=inputs,
+            outputs=outputs,
+            locktime=tx.tx.nLockTime,
+            version=tx.tx.nVersion,
+        )
+
+        # Fill signatures
+        if len(sigs) != len(tx.inputs()):
+            raise Exception("Incorrect number of inputs signed.")  # Should never occur
+        signatures = [
+            bh2u(ecc.der_sig_from_sig_string(x[1], ecc.CURVE_ORDER)) + "01"
+            for x in sigs
+        ]
+        tx.update_signatures(signatures)
+
         return "lol"
 
     def sign_message(self, message, keypath):
@@ -160,7 +277,7 @@ class Bitbox02Client(HardwareWalletClient):
         elif p2sh_p2wpkh and address_keypath[0] == 49 + HARDENED:
             script = bitbox02.btc.SCRIPT_P2WPKH_P2SH
         else:
-            raise Exception("invalid keypath or address_type, only supports BIP 84 p2wpkh and BIP 49 p2wpkh_p2sh")
+            raise Exception("invalid keypath or address_type, only supports BIP 84 p2wpkh bech32 addresses and BIP 49 p2wpkh_p2sh sript hash addresses")
 
         address = self.app.btc_pub(
             keypath=address_keypath,
